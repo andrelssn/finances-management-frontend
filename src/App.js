aed2e6f225b1c0991120ced2af2a7a1bcdcc96c7
André Santos <andreluissilsan@hotmail.com>
2025-07-29 01:36:00 -0300

    feat: header first idea

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useMediaQuery, useTheme } from '@mui/material';
+
+// Style
 import "./App.css";
 
 // Components
@@ -5,9 +8,12 @@ import Header from "./Router/Layout/Header";
 import MainRouter from "./Router/MainRouter";
 
 function App() {
+	const theme = useTheme();
+	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
 	return (
 		<div className="App">
-			<Header/>
+			<Header isMobile={isMobile}/>
 
 			<MainRouter/>
 		</div>
--- src/Router/Layout/Header.js
+++ src/Router/Layout/Header.js
@@ -1,9 +1,82 @@
+import React from "react";
+import { Box, Drawer, IconButton, List, ListItemButton, Tab, Tabs } from "@mui/material";
+
+// IMG & Icons
+import MenuIcon from '@mui/icons-material/Menu';
+import logo from '../../Images/FinancesLogo.png';
+
+// Style
 import "./Style.css";
 
-export default function Header() {
+// Components
+import { tabsSx, tabsSxMain } from "../../Components/Styles/TabsSx";
+
+export default function Header(props) {
+    const {
+        isMobile
+    } = props;
+
+    const [tabValue, setTabValue] = React.useState(0);
+    const [open, setOpen]         = React.useState(false);
+
+    const handleChangeTab = (event, newValue) => {
+        setTabValue(newValue);
+    };
+
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
+    };
+
+    const DrawerList = (
+        <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+            <List>
+                <ListItemButton href="/">
+                    Painel Principal
+                </ListItemButton>
+            </List>
+        </Box>
+    );
+
+    if (isMobile) {
+        return (
+            <header className="header-mobile">
+                <Box sx={{ position: "absolute", left: 5 }}>
+                    <IconButton onClick={toggleDrawer(true)}>
+                        <MenuIcon/>
+                    </IconButton>
+
+                    <Drawer open={open} onClose={toggleDrawer(false)}>
+                        {DrawerList}
+                    </Drawer>
+                </Box>
+
+                <div>
+                    <img
+                        alt="finances-logo"
+                        src={logo}
+                        style={{ width: 100 }}
+                    />
+                </div>
+            </header>
+        )
+    }
+
     return (
         <header className="header">
-            header
+            <div>
+                <img
+                    alt="finances-logo"
+                    src={logo}
+                    style={{ width: 100 }}
+                />
+            </div>
+
+            <Box>
+                <Tabs value={tabValue} onChange={handleChangeTab} sx={tabsSxMain}>
+                    <Tab label="Painel Principal" sx={tabsSx}/>
+                    <Tab label="Perfil" sx={tabsSx}/>
+                </Tabs>
+            </Box>
         </header>
     );
 }
